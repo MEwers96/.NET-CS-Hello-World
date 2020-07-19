9237421ee077232534baa76eb61e8de866e26703
Mike Ewers <creativedevvers@gmail.com>
2020-07-18 20:34:24 -0600

    Refactored code to follow common conventions found in the MS documentation.

--- Queue.cs
+++ Queue.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 
-namespace myQueue { 
+namespace HelloWorld{ 
 public class Queue
 {
-    StringEntry firstInLine = null;
-    StringEntry backOfLine = null;
+    private StringEntry _firstInLine = null;
+    private StringEntry _backOfLine = null;
 
 
     /* Function: add(String s)
@@ -14,20 +14,20 @@ public class Queue
      * The add function will create a new StringEntry with the parameter given. If there is no
      * first item in the queue, it will then set both the back and front to the queue. 
      */
-    public void push(String s)
+    public void Push(string s)
     {
 
-        if (backOfLine == null)
+        if (_backOfLine == null)
         {
-            StringEntry firstEntry = new StringEntry(s);
-            firstInLine = firstEntry;
-            backOfLine = firstEntry;
+            var firstEntry = new StringEntry(s);
+            _firstInLine = firstEntry;
+            _backOfLine = firstEntry;
         }
 
         else
         {
-            StringEntry entry = new StringEntry(s, backOfLine);
-            backOfLine = entry;
+            var entry = new StringEntry(s, _backOfLine);
+            _backOfLine = entry;
         }
 
     }
@@ -37,53 +37,63 @@ public class Queue
      * Parameters: NONE
      * 
      * Description:
-     * The pop function will take the data from the firstInLine StringEntry item, and then return the results.
-     * If the current firstInLine item is null, then the function will write to the console with an err message.
+     * The pop function will take the data from the _firstInLine StringEntry item, and then return the results.
+     * If the current _firstInLine item is null, then the function will write to the console with an err message.
      */
-    public String pop()
+    public string Pop()
         {
-            String result;
-            if(firstInLine == null){
+            string result;
+            if(_firstInLine == null){
 
                 Console.WriteLine("There are no items in the queue. Please use the 'push' function to queue a new item");
                 result = "NULL";    
             }
             else{
 
-                result = firstInLine.data;
-                firstInLine = firstInLine.next;
+                result = _firstInLine.GetData();
+                _firstInLine = _firstInLine.GetNext();
             }
                 
             return result;
 
         }
 
-    class StringEntry
+    public class StringEntry
     {
-        public String data;
-        StringEntry previous;
-        public StringEntry next = null;
+        readonly string _data;
+        private StringEntry _previous;
+        private StringEntry _next = null;
 
-        /* Function StringEntry(String s, StringEntry backOfLine = null) [Constructer]
+        /* Function StringEntry(String s, StringEntry _backOfLine = null) [Constructer]
          * Parameters: String s -> a string to be assigned to a StringEntry item
-         *             StringEntry backOfLine = null -> Optional parameter, used to set both previous and next attributes of a StringEntry. 
+         *             StringEntry _backOfLine = null -> Optional parameter, used to set both _previous and _next attributes of a StringEntry. 
          *
          * Description: 
-         * The StringEntry constructor will set the data of the item, and will set the "previous" field to the backOfLine StringEntry. 
-         * It then will go into the "previous" StringEntry and set its "next" field to the current StringEntry item. 
+         * The StringEntry constructor will set the _data of the item, and will set the "_previous" field to the _backOfLine StringEntry. 
+         * It then will go into the "_previous" StringEntry and set its "_next" field to the current StringEntry item. 
          */
-        public StringEntry(String s, StringEntry backOfLine = null)
+        public StringEntry(string s, StringEntry backOfLine = null)
         {
-            this.data = s;
+            this._data = s;
 
-           _ = backOfLine == null ? this.previous = null : previous = backOfLine;
+           _ = backOfLine == null ? this._previous = null : _previous = backOfLine;
 
 
-            if (this.previous != null)
+            if (this._previous != null)
             {
-                this.previous.next = this;
+                this._previous._next = this;
             }
 
+        }
+
+        public string GetData()
+        {
+            return this._data;
+        }
+
+        public StringEntry GetNext()
+        {
+            return this._next;
         }
 
     }
